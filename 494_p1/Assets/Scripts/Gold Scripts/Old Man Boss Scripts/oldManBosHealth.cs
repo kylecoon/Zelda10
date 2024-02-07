fb5efb61e96e0e1e021cf99b17f604e28d5c1907
nogivner <nogivner@umich.edu>
2024-02-07 06:31:41 -0500

    old man boss done

--- 494_p1/Assets/Scripts/Gold Scripts/Old Man Boss Scripts/oldManBosHealth.cs
+++ 494_p1/Assets/Scripts/Gold Scripts/Old Man Boss Scripts/oldManBosHealth.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class oldManBosHealth : MonoBehaviour
 {
 
-       public int health = 12;
+    public int health = 12;
     // Start is called before the first frame update
 
     public bool isDead = false;
@@ -40,13 +41,12 @@ public class oldManBosHealth : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 
     IEnumerator DamageColor() {
-        Sprite hold =  GetComponent<SpriteRenderer>().sprite;
+
+        //NES - The Legend of Zelda - NPCs_scaled_17x_pngcrushed
+        // Sprite hold = GetComponent<SpriteRenderer>().sprite;
         GetComponent<SpriteRenderer>().sprite = hurtSprites[0];
         yield return new WaitForSeconds(0.1f);
         GetComponent<SpriteRenderer>().sprite = hurtSprites[1];
@@ -55,51 +55,76 @@ public class oldManBosHealth : MonoBehaviour
         yield return new WaitForSeconds(0.1f);
         GetComponent<SpriteRenderer>().sprite = hurtSprites[3];
         yield return new WaitForSeconds(0.1f);
-        GetComponent<SpriteRenderer>().sprite = hold;
+        GetComponent<SpriteRenderer>().sprite = hurtSprites[4];
     }
 
-     public void AlterHealth(int health_change) {
+    public AudioClip LastSound;
+     public IEnumerator AlterHealth(int health_change) {
         
-        AudioSource.PlayClipAtPoint(damageSound, Camera.main.transform.position);
+        if(health == 1){
+             AudioSource.PlayClipAtPoint(LastSound, Camera.main.transform.position);
+        } else {
+            AudioSource.PlayClipAtPoint(damageSound, Camera.main.transform.position);
+        }
+        
         health += health_change;
-        
+
+        GetComponent<BoxCollider>().enabled = false;
+
         StartCoroutine(DamageColor());
         if (health <= 0 && gameObject != null) {
             isDead = true;
             StartCoroutine(Die());
         }
+        GetComponent<BoxCollider>().enabled = true;
+
+        yield return null;
     }
 
+
+    public GameObject egg;
     IEnumerator Die(){
         coroutineQueue.Clear();
 
+        GetComponent<TypeWriter>().DoSentences();
+
         StartCoroutine(shudder());
 
+        yield return new WaitForSeconds(5); // children will be gone by then
+
+        GameObject Egg = Instantiate(egg, gameObject.transform.position, Quaternion.identity);
+
+        CoroutineUtilities.MoveObjectOverTime(Egg.transform, Egg.transform.position, new Vector2(67,45), 5);
+
+        GetComponent<SpriteRenderer>().sprite = GetComponent<OldManBossAttack>().broken;
+
+        yield return new WaitForSeconds(0.3f);
+
+        Destroy(gameObject);
         //death stuff here
-        yield return null;
+        // yield return null;
     }
 
     void OnCollisionEnter(UnityEngine.Collision collision){
         if(collision.collider.CompareTag("Attack")){
             StartCoroutine(showShield());
 
-        } else if(collision.collider.CompareTag("Enemy")){
-            AlterHealth(-1);
-        }
+        } 
     }
 
     IEnumerator showShield(){
         shield.SetActive(true);
-        yield return new WaitForSeconds(0.4f);
+        yield return new WaitForSeconds(0.5f);
         shield.SetActive(false);
     }
 
     IEnumerator shudder(){
-        transform.position = new Vector2(transform.position.x + 0.1f, transform.position.y);
+        transform.position = new Vector2(transform.position.x, transform.position.y - 0.25f);
         while(true){
-            transform.position = new Vector2(transform.position.x - 0.2f, transform.position.y);
-            yield return new WaitForSeconds(0.01f);
-            transform.position = new Vector2(transform.position.x + 0.2f, transform.position.y);
+            transform.position = new Vector2(transform.position.x, transform.position.y + 0.5f);
+            yield return new WaitForSeconds(0.05f);
+            transform.position = new Vector2(transform.position.x, transform.position.y - 0.5f);
+
         }
         
     }
